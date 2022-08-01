63ad94cb9105c612db1a874da3723a3ad1a18b78
Lambodar Thakur <lthakur@primesoft.net>
2022-08-01 10:06:47 +0530

    first commit

---
+++ src/main/java/com/springrest/springrest/service/EmployeeService.java
@@ -0,0 +1,13 @@
+package com.springrest.springrest.service;
+
+import java.util.List;
+
+import com.springrest.springrest.entities.Employee;
+
+public interface EmployeeService {
+	
+	public List<Employee> getAllEmployees();
+	
+	public Employee getAllEmployee(long employeeId);
+
+}
---
+++ src/main/java/com/springrest/springrest/service/EmployeeServiceImpl.java
@@ -0,0 +1,41 @@
+package com.springrest.springrest.service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.springrest.springrest.entities.Employee;
+
+@Service
+public class EmployeeServiceImpl implements EmployeeService{
+	
+	List<Employee> emp;
+
+	public EmployeeServiceImpl() {
+		emp=new ArrayList<>();
+		emp.add(new Employee(1, "Java Dev", "Working As JAva"));
+		emp.add(new Employee(2, ".Net Dev", "Working As .Net"));
+	}
+
+	@Override
+	public List<Employee> getAllEmployees() {
+		// TODO Auto-generated method stub
+		
+		return emp;
+	}
+
+	@Override
+	public Employee getAllEmployee(long employeeId) {
+		// TODO Auto-generated method stub
+		Employee e=null;
+		for (Employee employee : emp) {
+			if(employee.getId()==employeeId) {
+				e=employee;
+				break;
+			}
+		}
+		return e;
+	}
+
+}
